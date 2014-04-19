3415abb0dd51431e204cf0295ff376d2ebab2baa
reaganjava@gmail.com <reaganjava@gmail.com>
2014-04-19 16:07:49 +0800

    v

---
+++ src/main/java/com/reagan/views/dto/Tip.java
@@ -0,0 +1,43 @@
+package com.reagan.views.dto;
+
+public class Tip {
+	
+	private static ResultObject<Object> tipMeg(String msg, boolean isValue) {
+		ResultObject<Object> result = new ResultObject<Object>();
+		result.setMessage(msg);
+		result.setSuccess(isValue);
+		return result;
+	}
+
+	public static ResultObject<Object> addSuccess() {
+		return tipMeg("添加成功", true);
+	}
+	
+	public static ResultObject<Object> editSuccess() {
+		return tipMeg("修改成功", true);
+	}
+	
+	public static ResultObject<Object> removeSuccess() {
+		return tipMeg("删除成功", true);
+	}
+	
+	public static ResultObject<Object> addFailure() {
+		return tipMeg("系统异常添加失败", true);
+	}
+	
+	public static ResultObject<Object> editFailure() {
+		return tipMeg("系统异常修改失败", true);
+	}
+	
+	public static ResultObject<Object> removeFailure() {
+		return tipMeg("系统异常删除失败", true);
+	}
+	
+	public static ResultObject<Object> loginOuttime() {
+		return tipMeg("登录超时", true);
+	}
+	
+	public static ResultObject<Object> passwordError() {
+		return tipMeg("密码错误修改失败", true);
+	}
+}
